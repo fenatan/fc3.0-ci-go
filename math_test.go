102bcd202f9e45987e30cb271b29caa45bba215d
Felipe Natan <felipenatan@novaandradina.org>
2022-04-11 23:21:15 -0400

    fix: test

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := soma(1, 2)
+	total := Soma(1, 2)
 
 	if total != 3 {
 		t.Errorf("Resultado da soma inválido: Resultado %d Esperado: %d", total, 3)
